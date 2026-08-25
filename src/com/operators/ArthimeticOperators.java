6852736434c2dba992fbabe060054af98d68d556
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-08-25 19:56:11 +0530

    Added code for Arithmetic Operators

---
+++ src/com/operators/ArthimeticOperators.java
@@ -0,0 +1,23 @@
+package com.operators;
+
+public class ArthimeticOperators {
+
+	public static void main(String[] args) {
+	
+		// Mathematical Operators : +,-,*,/,%,
+		int number1 = 10;
+		int number2 = 20;
+		double result;
+		result = number1 + number2;
+		System.out.println(result);
+		result = number1 - number2;
+		System.out.println(result);
+		result = number1*number2;
+		System.out.println(result);
+		result = ((double)number1)/number2;
+		System.out.println(result);
+		result = number1 % number2;
+		System.out.println(result);    
+		
+	}
+}
