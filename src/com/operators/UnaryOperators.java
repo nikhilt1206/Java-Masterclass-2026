c04768ea05acaa2580206fab27a2738cb241b3db
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-08-27 00:25:42 +0530

    Added code for Unary Operators

---
+++ src/com/operators/UnaryOperators.java
@@ -0,0 +1,28 @@
+package com.operators;
+
+public class UnaryOperators {
+
+	public static void main(String[] args) {
+		
+		int a; //declaration
+		a = +5; //assignment - positive number (+5 or 5 (preferred) both are same)
+		int b;
+		b = -a; 
+		System.out.println("a: " + a);
+		System.out.println("b: " + b);
+		
+		int c;
+		c = --a; //Pre-Decrement - Decrement first then use it
+		System.out.println("c: " + c);
+		System.out.println("a: " + a);
+		c = ++a; //Pre-Increment - Increment first then use it 
+		System.out.println("c: " + c);
+		System.out.println("a: " + a);
+		c = a--; //Post-Decrement - Use first then decrement it
+		System.out.println("c: " + c);
+		System.out.println("a: " + a);
+		c = a++; //Post-Increment - Use first then increment it
+		System.out.println("c: " + c);
+		System.out.println("a: " + a);
+	}
+}
