477a30dcc6eca3f94a8d81be8fdcd6f3c1dc3c8b
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-08-31 10:12:33 +0530

    Added code for Ternary Operator

---
+++ src/com/operators/TernaryOperator.java
@@ -0,0 +1,19 @@
+package com.operators;
+
+public class TernaryOperator {
+    public static void main(String[] args){
+        //Ternary operator - shorthand if-else statement (conditional statement)
+        //Expression - (Condition) ? expression1 : expression2 - if Condition is true - expression1 gets executed otherwise expression2
+        //One liner if-else statement
+
+        int a = 100;
+        int b = 120;
+        int max = (a>b) ? a : b; //max = a - since b is greater than a so 120 gets assigned to max variable
+        System.out.println(max);
+
+        //Number is even or odd
+        int number = 25;
+        boolean isEven = (number % 2 == 0) ? true : false; //since (25 % 2)!= 0 so it will print false
+        System.out.println(isEven);
+    }
+}
