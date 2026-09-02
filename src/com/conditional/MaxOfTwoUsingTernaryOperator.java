d6e952f17b8621feaa8e7192a6318ee28593d50f
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-09-02 23:58:17 +0530

    Added code for if-else statement

---
+++ src/com/conditional/MaxOfTwoUsingTernaryOperator.java
@@ -0,0 +1,10 @@
+package com.conditional;
+
+public class MaxOfTwoUsingTernaryOperator {
+    public static void main(String[] args){
+        int num1 = 54;
+        int num2 = 44;
+        int max = (num1>num2)?num1:num2;
+        System.out.println("Max is: "+max);
+    }
+}
